2e81d460ce5c2bdba094ee9f8a49ebcf05eb56b7
dasanchez11 <diegosanchez@Diegos-MacBook-Pro.local>
2023-05-02 15:20:52 -0500

    fix icons y githubapi

--- githubFolder/util/githubFetch.js
+++ githubFolder/util/githubFetch.js
@@ -1,57 +1,69 @@
-export class GithubFetch{
+export class GithubFetch {
+  constructor() {
+    // this.cliendId = '19558fcfc7a4c4dbf5a4'
+    // this.clientSecret = 'ae1be83efbc973710f237951867a0ba3b4596255'
+    this.baseUrl = "https://api.github.com/";
+    // this.authUrl = `client_id=${this.cliendId}&client_secret=${this.clientSecret}`
+    this.authToken = "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";
+  }
 
-    constructor(){
-        // this.cliendId = '19558fcfc7a4c4dbf5a4'
-        // this.clientSecret = 'ae1be83efbc973710f237951867a0ba3b4596255'
-        this.baseUrl = 'https://api.github.com/'
-        // this.authUrl = `client_id=${this.cliendId}&client_secret=${this.clientSecret}`
-        this.authToken = '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'
-    }
+  getUsers = async (searchQuery, page = 1, per_page = 5) => {
+    const pagination = `&page=${page}&per_page=${per_page}`;
+    const response = await fetch(
+      this.baseUrl + `search/users?q=${searchQuery}` + pagination
+    );
+    const users = await response.json();
+    return users;
+  };
 
-    getUsers = async (searchQuery,page=1,per_page=5) =>{
-        const pagination = `&page=${page}&per_page=${per_page}`
-        const response = await fetch(this.baseUrl+`search/users?q=${searchQuery}`+pagination)
-        const users = await response.json()
-        return users
-    }
+  getUser = async (searchUser) => {
+    const res = await fetch(this.baseUrl + `users/${searchUser}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.authToken}`,
+      },
+    });
+    const user = await res.json();
+    return user;
+  };
 
-    getUser = async (searchUser) =>{
-        const res = await fetch(this.baseUrl+`users/${searchUser}`,{
-            method: 'GET',
-            headers:{"Content-Type": "application/json" ,
-            'Authorization': `Bearer ${this.authToken}`}})
-        const user = await res.json()
-        return user
-    }
+  getRepositories = async (searchUser, page = 1, per_page = 15) => {
+    const pagination = `page=${page}&per_page=${per_page}&sort=updated-asc`;
+    const res = await fetch(
+      this.baseUrl + `users/${searchUser}/repos` + "?q=" + pagination,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.authToken}`,
+        },
+      }
+    );
+    const user = await res.json();
+    return user;
+  };
 
-    getRepositories = async(searchUser,page=1,per_page=15)=>{
-        const pagination = `page=${page}&per_page=${per_page}&sort=updated-asc`
-        const res = await fetch(this.baseUrl+`users/${searchUser}/repos`+'?q='+pagination,{
-            method: 'GET',
-            headers:{"Content-Type": "application/json" ,
-            'Authorization': `Bearer ${this.authToken}`}})
-        const user = await res.json()
-        return user
-    }
+  debouce = (callback, delay = 700) => {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
+  };
 
-    debouce = (callback, delay=700) =>{
-        let timer
-        return (...args) =>{
-            clearTimeout(timer)
-            timer = setTimeout(()=>{
-                callback(...args)
-            },delay)
-        }
-    }
-
-    getStars = async(userId) =>{
-        const url = `${this.baseUrl}users/${userId}/starred`
-        const res = await fetch(url,{
-            method: 'GET',
-            headers:{"Content-Type": "application/json" ,
-            'Authorization': `Bearer ${this.authToken}`}})
-        const stars = await res.json()
-        return stars
-    }
+  getStars = async (userId) => {
+    const url = `${this.baseUrl}users/${userId}/starred`;
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.authToken}`,
+      },
+    });
+    const stars = await res.json();
+    return stars;
+  };
 }
-
